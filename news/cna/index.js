60d101136614e7c042464722f4f5b5f0533b53be
潘冠辰 <91081800+louis4116@users.noreply.github.com>
2023-09-08 19:00:36 +0800

    updated

--- news/cna/index.js
+++ news/cna/index.js
@@ -1,20 +1,21 @@
 // const { chromium } = require("playwright");
 const puppeteer = require("puppeteer");
 const cnaScrap = async (id) => {
-  const browser = await puppeteer.launch({
-    headless: "news",
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
   try {
+    const browser = await puppeteer.launch({
+      headless: "news",
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
+
     let page = await browser.newPage();
     // await page.setRequestInterception(true);
     // page.on("request", (request) => {
